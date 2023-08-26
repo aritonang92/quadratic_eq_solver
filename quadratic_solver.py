156cfa277d8cda3885316974218d68969084e7a5
dhaniel <65146994+aritonang92@users.noreply.github.com>
2023-08-26 18:24:42 +0700

    Update quadratic_solver.py

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -13,9 +13,7 @@ import streamlit as st
 st.title("A Simple Quadratic Equation Solver")
 
 st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
+    $x = \frac{-b\pm\sqrt{b^2 - 4ac}}{2a}$
     ''')
 
 name = st.text_input("Enter your name"," ")
